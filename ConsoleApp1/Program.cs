edda3dd528fe6ce418c2dca5b130cb6d74cb384f
kharadeshivprasad <162097492+kharadeshivprasad@users.noreply.github.com>
2024-03-13 19:17:05 +0800

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Shivprasad Kharade, Well going");
+            Console.WriteLine("Hello Shivprasad Kharade, Well going!");
             Console.ReadKey();
         }
     }
